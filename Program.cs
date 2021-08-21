8c1bdc432672e3d0044734066c4e9d4492f7692f
Chris Wong <wc38.wong@gmail.com>
2021-08-21 10:37:06 +0800

    Done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.IO;
 
 namespace Example
 {
     class Program
     {
+        static int instances;
+        static string datpath = @"/var/log/application/dat";
+        static string varpath = @"/var/log/application/var";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string[] vs = GetVar();
+            foreach (string line in GetDat())
+            {
+                foreach (string v in vs)
+                {
+                    if (v.IndexOf(line, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        Console.WriteLine(line);
+                        instances++;
+                    }
+                }
+            }
+            Console.WriteLine($"instances = {instances}");
+        }
+
+        static string[] GetDat()
+        {
+            return File.ReadAllLines(datpath);
+        }
+
+        static string[] GetVar()
+        {
+            return File.ReadAllLines(varpath);
         }
     }
 }
